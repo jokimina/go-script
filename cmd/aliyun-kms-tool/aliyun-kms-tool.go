f72d3abd4fba7cd5c2fbe59a188f05bc6cbfaf35
xiaodong <jokimina@163.com>
2018-11-01 09:36:03 +0800

    add usage

--- cmd/aliyun-kms-tool/aliyun-kms-tool.go
+++ cmd/aliyun-kms-tool/aliyun-kms-tool.go
@@ -38,6 +38,17 @@ func init() {
 	flag.StringVar(&encrypt, "e", "", "encrypt")
 	flag.StringVar(&decrypt, "d", "", "decrypt")
 	flag.StringVar(&kmsKeyId, "id", "", "kms key id")
+	flag.Usage = usage
+}
+
+func usage() {
+		fmt.Printf( `
+  encrypt:
+    %[1]s -e xxxxxxxxx -id xxxxxx
+  decrypt:
+    %[1]s -d xxxxxxxxxx
+`, os.Args[0])
+		flag.PrintDefaults()
 }
 
 func Decrypt(kmsClient *kms.Client, cipherTextBlob string) (plaintText string) {
@@ -67,11 +78,13 @@ func main() {
 	flag.Parse()
 	if (encrypt == "" && decrypt == "") || (encrypt != "" && decrypt != "") {
 		flag.Usage()
-		panic("decrypt or encrypt? ")
+		fmt.Println("decrypt or encrypt? ")
+		os.Exit(1)
 	}
 	if decrypt == "" && (encrypt == "" && kmsKeyId == "") {
 		flag.Usage()
-		panic("encrypt need a kms key id !")
+		fmt.Println("encrypt need a kms key id !")
+		os.Exit(1)
 	}
 	config := getConfigFromEnv()
 	kmsClient, err := kms.NewClientWithAccessKey(config.RegionId, config.AccessKeyId, config.AccessKeySecret)
