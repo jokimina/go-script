a840e93201c318e40cd44c232f52c4bbeca6c826
xiaodong <jokimina@163.com>
2018-11-08 07:24:19 +0800

    [Upgrade] pholcus cmd options

--- cmd/pholcus_aliyun_bill/pholcus_aliyun_bill.go
+++ cmd/pholcus_aliyun_bill/pholcus_aliyun_bill.go
@@ -6,6 +6,8 @@ import (
 	_ "github.com/jokimina/go-script/pkg/spider_lib" // 同样你也可以自由添加自己的规则库
 )
 
+//cmd options:
+// -_ui=cmd -a_mode=0 -c_spider=0 -a_outtype=csv -a_thread=20 -a_dockercap=5000 -a_pause=300 -a_proxyminute=0 -a_keyins="<pholcus><golang>" -a_limit=10 -a_success=true -a_failure=true
 func main() {
 	// 设置运行时默认操作界面，并开始运行
 	// 运行软件前，可设置 -a_ui 参数为"web"、"gui"或"cmd"，指定本次运行的操作界面
