6b506350c6eee7252901d2851da080dcaf009839
xiaodong <jokimina@163.com>
2018-11-29 09:23:15 +0800

    update jenkins demo && trim chinese char

--- cmd/jenkins_batch_update/jenkins_batch_update.go
+++ cmd/jenkins_batch_update/jenkins_batch_update.go
@@ -14,36 +14,41 @@ func main() {
 		panic(err)
 	}
 	//job, err := jenkins.GetJob("test/job/mars/job/sc-aql")
-	job, err := jenkins.GetJob("test/job/mars")
+	job, err := jenkins.GetJob("platform/job/mars")
 	if err != nil {
 		panic(err)
 	}
 
-	managedShellId := "7e260101-25d7-46b8-8a57-9c25fe9a6e4a"
+	managedShellId := "bb48c392-2bf9-49e0-bae9-83b3ab1f5ad0"
 	jobs, err := job.GetInnerJobs()
 	for _, j := range jobs {
 		jobName := j.GetName()
-		if strings.HasPrefix(jobName, "sc-") && !strings.Contains(jobName, "eureka") {
-			fmt.Println("try update job " + jobName)
-			tJob, err := jenkins.GetJob("test/job/mars/job/" + jobName)
+		if strings.HasPrefix(jobName, "sc-") && !strings.Contains(jobName, "eureka") && !strings.Contains(jobName, "web") {
+			tJob, err := jenkins.GetJob("platform/job/mars/job/" + jobName)
 			if err != nil {
 				panic(err)
 			}
 			c, err := tJob.GetConfig()
+			if strings.Contains(c, managedShellId) {
+				continue
+			}
 			lines := strings.Split(c, "\n")
 			re := regexp.MustCompile("(>)(.*)(<)")
 			for i, line := range lines {
-				if strings.Contains(line, "<buildStepId>") && !strings.Contains(line, managedShellId) {
+				if strings.Contains(line, "<buildStepId>") {
+					fmt.Println("try update job " + jobName + ": uat/job/mars/job/" + jobName)
 					lines[i] = re.ReplaceAllString(line, fmt.Sprintf("${1}%s${3}", managedShellId))
+					break
 				}
 			}
-			newConfig := strings.Join(lines, "\n")
+			// 删除中文
+			newConfig := regexp.MustCompile("[\u4e00-\u9fa5]+").ReplaceAllString(strings.Join(lines, "\n"), "")
 			//fmt.Println(newConfig)
 			err = tJob.UpdateConfig(newConfig)
 			if err != nil {
 				panic(err)
 			}
-			break
+			//break
 		}
 	}
 
